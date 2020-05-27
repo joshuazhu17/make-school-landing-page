48ba05e89ea63c72d8a0c4b4085d6eeba0407735
joshuazhu17 <joshua.zhu@gmail.com>
2020-05-26 19:52:56 -0700

    Attempted to add navbar hamburger menu Didn't work

---
+++ src/scss/palm/navbar.ts
@@ -0,0 +1,11 @@
+/*
+import "./navbar.scss";
+
+function rightHamburgerMenu() {
+    const navitems = document.querySelectorAll('.right-items > .nav-item');
+
+    navitems.forEach(nav => nav.classList.toggle('toggle-show'));
+}
+
+document.querySelector('.right-menu').addEventListener('click', rightHamburgerMenu);
+*/
