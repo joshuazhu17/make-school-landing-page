53267ea8e0115ab123dfc0730762a674bbe213f2
joshuazhu17 <joshua.zhu@gmail.com>
2020-05-26 20:59:39 -0700

    Got hamburger menu working

--- src/scss/palm/navbar.ts
+++ src/scss/palm/navbar.ts
@@ -1,5 +1,3 @@
-/*
-import "./navbar.scss";
 
 function rightHamburgerMenu() {
     const navitems = document.querySelectorAll('.right-items > .nav-item');
@@ -7,5 +5,16 @@ function rightHamburgerMenu() {
     navitems.forEach(nav => nav.classList.toggle('toggle-show'));
 }
 
+function resetView() {
+    const navitems = document.querySelectorAll('.right-items > .nav-item');
+
+    navitems.forEach(nav => nav.classList.toggle('toggle-show', false));
+}
+
+function korok() {
+    alert("YAHAHA! YOU FOUND ME!");
+    // my go-to quick debugging function
+}
+
 document.querySelector('.right-menu').addEventListener('click', rightHamburgerMenu);
-*/
+window.addEventListener('resize', resetView);
